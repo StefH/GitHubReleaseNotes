ea770cac7bf78b5fcbb4aa3cb4af6324dad02352
Stef Heyenrath <stef.heyenrath@gmail.com>
2020-05-24 11:22:14 +0200

    netcore

---
+++ src/GitHubReleaseNotes.NETCore3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHubReleaseNotes.NETCore3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
